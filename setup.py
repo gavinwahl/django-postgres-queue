9fb88b095305fb68b2c59ea865cc1aefbb760a1d
Gavin Wahl <gwahl@fusionbox.com>
2017-09-11 16:22:54 -0600

    document requirements

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup
 setup(
     name='django-postgres-queue',
     version='0.2.0.dev0',
-    packages=['dpq',],
+    packages=['dpq'],
     license='BSD',
     long_description=open('README.rst').read(),
     author="Gavin Wahl",
     author_email="gavinwahl@gmail.com",
     url="https://github.com/gavinwahl/django-postgres-queue",
+    install_requires=[
+        'Django>=1.11',
+    ]
 )
