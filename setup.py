bd614bc58f3116d94fd69d0ca9f622511537a350
Gavin Wahl <gwahl@fusionbox.com>
2019-03-19 15:24:30 -0600

    Preparing release 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-postgres-queue',
-    version='0.2.2.dev0',
+    version='0.3.0',
     packages=['dpq', 'dpq.migrations'],
     license='BSD',
     long_description=open('README.rst').read(),
