d841683185e81bf4dd104b041e65fb7681b36b01
Gavin Wahl <gwahl@fusionbox.com>
2019-04-04 12:53:55 -0600

    Retry with exponential backoff decorator

--- dpq/decorators.py
+++ dpq/decorators.py
@@ -1,3 +1,8 @@
+import datetime
+import logging
+import random
+
+
 def repeat(delay):
     """
     Endlessly repeats a task, every `delay` (a timedelta).
@@ -22,5 +27,37 @@ def repeat(delay):
             )
             return fn(queue, job)
 
+        return inner
+    return decorator
+
+
+def exponential_with_jitter(offset=6):
+    def delayfn(retries):
+        jitter = random.randrange(-15, 15)
+        return datetime.timedelta(seconds=2 ** (retries + offset) + jitter)
+    return delayfn
+
+
+def retry(max_retries, delayfn=exponential_with_jitter(), Exc=Exception):
+    def decorator(fn):
+        logger = logging.getLogger(__name__)
+
+        def inner(queue, job):
+            try:
+                return fn(queue, job)
+            except Exc as e:
+                retries = job.args.get('retries', 0)
+                if retries < max_retries:
+                    job.args['retries'] = retries + 1
+                    delay = delayfn(retries)
+                    job.execute_at += delay
+                    job.save(force_insert=True)
+                    logger.warning(
+                        'Task %r failed: %s. Retrying in %s.', job, e, delay, exc_info=True,
+                    )
+                else:
+                    logger.exception(
+                        'Task %r exceeded its retry limit: %s.', job, e, exc_info=True,
+                    )
         return inner
     return decorator
