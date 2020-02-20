714b3c5c0989c837801ff6e54d373552c9f2ca50
Jervis Whitley <jtrain@users.noreply.github.com>
2020-02-20 04:41:46 +0000

    initial test case for adding a job on the queue.

--- dpq/tests.py
+++ dpq/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Job
+from .queue import AtLeastOnceQueue
+
+
+class DpqQueueTests(TestCase):
+    def demotask(queue, job):
+        return job.id
+
+    def test_create_job_on_queue(self):
+        """
+        Creates a basic queue with a name, and puts the job onto the queue.
+        """
+        NAME = "machine_a"
+        queue = AtLeastOnceQueue(tasks=["demotask"], queue=NAME)
+
+        queue.enqueue("demotask", {"count": 5})
+        job = Job.dequeue(queue=queue.queue)
+        self.assertEqual(job.args["count"], 5)
+        self.assertEqual(job.queue, NAME)
